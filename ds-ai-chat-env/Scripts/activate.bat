1426fc0f68fd42ae45e798b2cce7604bb7a9ddb9
Ana Carolina <karol.fdias@hotmail.com>
2024-12-01 11:09:22 -0300

    Changing computer virtuaenv

--- ds-ai-chat-env/Scripts/activate.bat
+++ ds-ai-chat-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\anacf.TAKENET\Desktop\Estudos\desafio-tecnico-ds-ai-chat\ds-ai-chat-env
+set VIRTUAL_ENV=D:\Repos-foradrive\Estudos\desafio-tecnico-ds-ai-chat\ds-ai-chat-env
 
 if not defined PROMPT set PROMPT=$P$G
 
